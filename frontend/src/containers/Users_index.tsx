4a7f32060c432242eb75f9636dce7b149d1b1a9c
ryuhei202 <ryuhei202@docomo.ne.jp>
2022-10-08 15:41:19 +0900

    Complete fech api from frontend

--- frontend/src/containers/Users_index.tsx
+++ frontend/src/containers/Users_index.tsx
@@ -1,8 +1,14 @@
 // ここではusers一覧を表示します。GET: /usersに反応
 
-import React from "react";
+import React, { useEffect } from "react";
+import { fetchUsers } from "../apis/users";
 
 export const Users = () => {
+
+  useEffect(() => {
+    fetchUsers().then((data) => console.log(data)
+    )
+  }, [])
   return (
 
     <p>ユーザー一覧ページです</p>
