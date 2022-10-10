d1c4ee39d734c41c4bfc408bc38312b1dedf34be
ryuhei202 <ryuhei202@docomo.ne.jp>
2022-10-11 02:11:28 +0900

    The mock-up of the show page has been completed.

--- frontend/src/containers/Users _show .tsx
+++ frontend/src/containers/Users _show .tsx
@@ -1,28 +1,54 @@
 import { match } from "assert";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchUsersShow } from "../apis/users_id";
+
+type Userstype = {
+  created_at: string;
+  email: string;
+  gender: string;
+  id: number;
+  name: string;
+  password_digest: string;
+  profile_image: string;
+  self_introduction: string;
+  updated_at: string;
+}
+
 
 
 
 export const UsersShow = () => {
+
+  const [state, setState] = useState<Userstype | null>(null);
 
   const  { id } = useParams<{ id: any }>();
 
 
   useEffect(() => {
     fetchUsersShow(id)
-    .then((data) =>
-      console.log(data)
+    .then((data) =>{
+      setState(data.user)
+    }
+     
     )
-  }, [])
+  }, []);
+
+
+ 
 
 
 
   return (
     <>
     <p>showページです</p>
-     <p>パラメーターは{id}です。</p>
+    <ul>
+      <li>名前:{state?.name}</li>
+      <li>email:{state?.name}</li>
+      <li>gender:{state?.gender}</li>
+      <li>self_introduction:{state?.self_introduction}</li>
+    </ul>
+     
     </>
   )
 };
