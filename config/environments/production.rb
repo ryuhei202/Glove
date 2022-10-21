0683d09f3dc765dbd965ff7d46fbcd46639b68bf
ryuhei202 <ryuhei202@docomo.ne.jp>
2022-10-21 21:34:26 +0900

    ok

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,8 +25,8 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
-  config.assets.compile = true
-config.assets.initialize_on_precompile=false
+#   config.assets.compile = true
+# config.assets.initialize_on_precompile=false
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
