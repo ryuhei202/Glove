3e64931e0681e9a63e7a237acb26124872425964
ryuhei202 <ryuhei202@docomo.ne.jp>
2022-10-21 22:20:43 +0900

    s

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,7 @@ require_relative "boot"
 require "rails/all"
 
 
-require "sprockets/railtie”
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
