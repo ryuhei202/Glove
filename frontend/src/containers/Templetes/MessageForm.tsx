87fdd59d76650c15049dc0dfe3444afe242b894b
ryuhei202 <ryuhei202@docomo.ne.jp>
2022-11-22 16:25:01 +0900

    change button color

--- frontend/src/containers/Templetes/MessageForm.tsx
+++ frontend/src/containers/Templetes/MessageForm.tsx
@@ -9,7 +9,7 @@ export const MessageForm = (props:any) => {
 
      <textarea className="border-4 mr-5 w-6/12" placeholder="メッセージを入力"  value={props.content} onChange={props.onChangeTextare}/>
 
-  <label className="shadow-lg px-2 py-1 h-10 w-10 text-xs bg-blue-400  text-white font-semibold rounded  hover:bg-blue-500 hover:shadow-sm hover:translate-y-0.5 transform transition ">
+  <label className="shadow-lg px-2 py-1 h-10 w-10 text-xs text-center  text-white font-semibold bg-blue-600 rounded-xl hover:bg-blue-700 hover:shadow-sm hover:translate-y-0.5 transform transition ">
      <p>img</p>
      <input className="hidden"
              accept="image/*"
@@ -19,7 +19,7 @@ export const MessageForm = (props:any) => {
              }}
            />
   </label>
-     <button className="mr-6 ml-auto shadow-lg px-2 py-1  bg-blue-400 text-lg text-white font-semibold rounded  hover:bg-blue-500 hover:shadow-sm hover:translate-y-0.5 transform transition " onClick={props.onSubmit} type="button">送信</button>
+     <button className="mr-6 ml-auto shadow-lg px-2 py-1  text-lg text-white font-semibold  bg-blue-600 rounded-xl hover:bg-blue-700 hover:shadow-sm hover:translate-y-0.5 transform transition " onClick={props.onSubmit} type="button">送信</button>
 </div>
  </form> 
   </>
