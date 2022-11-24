74dbca903b563fa3f17050cd1bdca0e4f5ec63dd
ryuhei202 <ryuhei202@docomo.ne.jp>
2022-11-24 13:07:57 +0900

    create database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,300 @@
-# 3.times do |n|
-#   user = User.new(
-#     name:"example_user_#{n}",
-#     email:"eampleuuser#{n+1}@gmail.com",
-#     language: "japanese",
-#     password:"foobar",
-#     password_confirmation:"foobar",
-#     gender:0
-#   )
+5.times do |n|
+  User.create!(
+    name:"chinese_user_#{n}",
+    email:"chinese#{n+1}@gmail.com",
+    language: "chinese",
+    password:"foobar",
+    password_confirmation:"foobar",
+    gender:0
+  )
+end
+
+5.times do |n|
+  User.create!(
+    name:"english_user_#{n}",
+    email:"english#{n+1}@gmail.com",
+    language: "english",
+    password:"foobar",
+    password_confirmation:"foobar",
+    gender:0
+  )
+end
+
+5.times do |n|
+  User.create!(
+    name:"russian_user_#{n}",
+    email:"russian#{n+1}@gmail.com",
+    language: "russian",
+    password:"foobar",
+    password_confirmation:"foobar",
+    gender:0
+  )
+end
+
+5.times do |n|
+  User.create!(
+    name:"romanian_user_#{n}",
+    email:"romanian#{n+1}@gmail.com",
+    language: "romanian",
+    password:"foobar",
+    password_confirmation:"foobar",
+    gender:0
+  )
+end
+
+5.times do |n|
+  User.create!(
+    name:"lithuania_user_#{n}",
+    email:"lithuania#{n+1}@gmail.com",
+    language: "lithuania",
+    password:"foobar",
+    password_confirmation:"foobar",
+    gender:0
+  )
+end
+
+5.times do |n|
+  User.create!(
+    name:"latvian_user_#{n}",
+    email:"latvian#{n+1}@gmail.com",
+    language: "latvian",
+    password:"foobar",
+    password_confirmation:"foobar",
+    gender:0
+  )
+end
+
+5.times do |n|
+  User.create!(
+    name:"portuguese_user_#{n}",
+    email:"portuguese#{n+1}@gmail.com",
+    language: "portuguese",
+    password:"foobar",
+    password_confirmation:"foobar",
+    gender:0
+  )
+end
+
+5.times do |n|
+  User.create!(
+    name:"polish_user_#{n}",
+    email:"polish#{n+1}@gmail.com",
+    language: "polish",
+    password:"foobar",
+    password_confirmation:"foobar",
+    gender:0
+  )
+end
+
+5.times do |n|
+  User.create!(
+    name:"bulgarian_user_#{n}",
+    email:"bulgarian#{n+1}@gmail.com",
+    language: "bulgarian",
+    password:"foobar",
+    password_confirmation:"foobar",
+    gender:0
+  )
+end
+
+5.times do |n|
+  User.create!(
+    name:"french_user_#{n}",
+    email:"french#{n+1}@gmail.com",
+    language: "french",
+    password:"foobar",
+    password_confirmation:"foobar",
+    gender:0
+  )
+end
+
+5.times do |n|
+  User.create!(
+    name:"finnish_user_#{n}",
+    email:"finnish#{n+1}@gmail.com",
+    language: "finnish",
+    password:"foobar",
+    password_confirmation:"foobar",
+    gender:0
+  )
+end
+
+5.times do |n|
+  User.create!(
+    name:"hungarian_user_#{n}",
+    email:"hungarian#{n+1}@gmail.com",
+    language: "hungarian",
+    password:"foobar",
+    password_confirmation:"foobar",
+    gender:0
+  )
+end
+
+5.times do |n|
+  User.create!(
+    name:"turkish_user_#{n}",
+    email:"turkish#{n+1}@gmail.com",
+    language: "turkish",
+    password:"foobar",
+    password_confirmation:"foobar",
+    gender:0
+  )
+end
+
+5.times do |n|
+  User.create!(
+    name:"german_user_#{n}",
+    email:"german#{n+1}@gmail.com",
+    language: "german",
+    password:"foobar",
+    password_confirmation:"foobar",
+    gender:0
+  )
+end
+
+5.times do |n|
+  User.create!(
+    name:"danish_user_#{n}",
+    email:"danish#{n+1}@gmail.com",
+    language: "danish",
+    password:"foobar",
+    password_confirmation:"foobar",
+    gender:0
+  )
+end
+
+5.times do |n|
+  User.create!(
+    name:"czech_user_#{n}",
+    email:"czech#{n+1}@gmail.com",
+    language: "czech",
+    password:"foobar",
+    password_confirmation:"foobar",
+    gender:0
+  )
+end
+
+5.times do |n|
+  User.create!(
+    name:"slovenian_user_#{n}",
+    email:"slovenian#{n+1}@gmail.com",
+    language: "slovenian",
+    password:"foobar",
+    password_confirmation:"foobar",
+    gender:0
+  )
+end
+
+5.times do |n|
+  User.create!(
+    name:"slovakian_user_#{n}",
+    email:"slovakian#{n+1}@gmail.com",
+    language: "slovakian",
+    password:"foobar",
+    password_confirmation:"foobar",
+    gender:0
+  )
+end
+
+5.times do |n|
+  User.create!(
+    name:"spanish_user_#{n}",
+    email:"spanish#{n+1}@gmail.com",
+    language: "spanish",
+    password:"foobar",
+    password_confirmation:"foobar",
+    gender:0
+  )
+end
+
+5.times do |n|
+  User.create!(
+    name:"swedish_user_#{n}",
+    email:"swedish#{n+1}@gmail.com",
+    language: "swedish",
+    password:"foobar",
+    password_confirmation:"foobar",
+    gender:0
+  )
+end
+
+5.times do |n|
+  User.create!(
+    name:"greek_user_#{n}",
+    email:"greek#{n+1}@gmail.com",
+    language: "greek",
+    password:"foobar",
+    password_confirmation:"foobar",
+    gender:0
+  )
+end
+
+5.times do |n|
+  User.create!(
+    name:"dutch_user_#{n}",
+    email:"dutch#{n+1}@gmail.com",
+    language: "dutch",
+    password:"foobar",
+    password_confirmation:"foobar",
+    gender:0
+  )
+end
+
+5.times do |n|
+  User.create!(
+    name:"estonian_user_#{n}",
+    email:"estonian#{n+1}@gmail.com",
+    language: "estonian",
+    password:"foobar",
+    password_confirmation:"foobar",
+    gender:0
+  )
+end
+
+5.times do |n|
+  User.create!(
+    name:"ukrainian_user_#{n}",
+    email:"ukrainian#{n+1}@gmail.com",
+    language: "ukrainian",
+    password:"foobar",
+    password_confirmation:"foobar",
+    gender:0
+  )
+end
+
+5.times do |n|
+  User.create!(
+    name:"indonesian_user_#{n}",
+    email:"indonesian#{n+1}@gmail.com",
+    language: "indonesian",
+    password:"foobar",
+    password_confirmation:"foobar",
+    gender:0
+  )
+end
+
+5.times do |n|
+  User.create!(
+    name:"italian_user_#{n}",
+    email:"italian#{n+1}@gmail.com",
+    language: "italian",
+    password:"foobar",
+    password_confirmation:"foobar",
+    gender:0
+  )
+end
+
+
+
+
+
+
+
+
+
 
 # User.create!(name:"1",email:"eampleuuser1@gmail.com",gender:0,language:"japanese",password:"foobard")
 #   user.save!
-# end
+
 
 
